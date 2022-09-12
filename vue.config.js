d79a5e4e3f9c0d3e8a0ee5d62f09caf3deab0f96
mesabuca <mehmet.buyukcaglayan@modanisa.com>
2022-09-13 01:09:31 +0300

    Vue config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/'
 })
 
-// module.exports = {
-//   publicPath: '/'
-// }
+module.exports = {
+  publicPath: '/'
+}
