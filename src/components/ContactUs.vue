be6e68da7bdc96a3bbb3d2b1892f6b98defd2e9b
mesabuca <mehmet.buyukcaglayan@modanisa.com>
2022-09-18 21:07:23 +0300

    Contact Us

---
+++ src/components/ContactUs.vue
@@ -0,0 +1,35 @@
+<template>
+<v-row class="mt-16">
+  <v-col cols="3"/>
+  <v-col cols="6">
+    <v-card
+        class="mx-auto"
+        outlined
+    >
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-list-item-title class="text-h5 mb-1">
+            Bize Ulaşın
+          </v-list-item-title>
+          Okumer Makine Endüstri Sanayi ve Ticaret Limited Şirketi <br>
+          okumermakine@gmail.com <br>
+          info@okumer.com <br>
+          +(90) 541 455 87 74 <br>
+          facebook twitter ve instagram adreslerimiz var knk :) <br>
+          Eskihisar Mh. 8021.Sk. No:8 Hurdacılar Sitesi/Merkezefendi/Denizli <br>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+  </v-col>
+</v-row>
+</template>
+
+<script>
+export default {
+  name: "ContactUs"
+}
+</script>
+
+<style scoped>
+
+</style>
