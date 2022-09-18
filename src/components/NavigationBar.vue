63402ae0a9ab4cb244eac524d495338764aa3264
mesabuca <mehmet.buyukcaglayan@modanisa.com>
2022-09-18 21:07:00 +0300

    Navigation Bar

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card class="overflow-hidden">
+    <v-app-bar
+        height="50"
+        color="white"
+        elevate-on-scroll
+        scroll-target="#scrolling-techniques-7"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "NavigationBar"
+}
+</script>
+
+<style scoped>
+
+</style>
