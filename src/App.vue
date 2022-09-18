27a69fdfd9c6599d2b2ffc11a0ddf9f92cb924aa
mesabuca <mehmet.buyukcaglayan@modanisa.com>
2022-09-18 21:06:22 +0300

    Index page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <NavigationBar/>
+      <HelloWorld class="mt-5"/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NavigationBar from "@/components/NavigationBar";
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
+    NavigationBar,
   },
 
   data: () => ({
