1cb9a4af344d7116dfbea73e71740844eb2befd8
mesabuca <mehmet.buyukcaglayan@modanisa.com>
2022-09-18 21:06:38 +0300

    Main Banner

---
+++ src/components/MainBanner.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+    <v-row class="align-center h-100 text-white">
+      <v-col class="text-center">
+        <h1 class="text-h4 font-weight-thin mb-4">
+          OkumerMakina
+        </h1>
+        <h4 class="subheading">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis corporis culpa cumque delectus deserunt ea iste laudantium maxime natus odio placeat, quibusdam quo reiciendis rem tempora. Aspernatur enim molestias non.
+        </h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
+
+<script>
+export default {
+  name: "MainBanner"
+}
+</script>
+
+<style scoped>
+
+</style>
